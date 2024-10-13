9d8d7d8b001afd41f0628d5c1894c941a4f8e2c1
noahsarktaiwan <ben109092ben@gmail.com>
2024-10-13 13:19:10 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <arduino.h>
+#include "motor.h"
 
 void setup() {
-
+    pinMode(motor1_1, OUTPUT);
+    pinMode(motor1_2, OUTPUT);
+    pinMode(motor2_1, OUTPUT);
+    pinMode(motor2_2, OUTPUT);
 }
 
 void loop() {
